d5963a14bae49f1ba848342cf2e2c4a77efa5ddf
Shravan Patel <shravanpatel5@gmail.com>
2018-06-06 23:43:47 +0530

    Added lib folder

---
+++ lib/mpj-v0_44/src/mpjdev/natmpjdev/lib/build/CMakeFiles/nativempjdev.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/mpjdev_natmpjdev_Comm.c" "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/build/CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_Comm.c.o"
+  "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/mpjdev_natmpjdev_Group.c" "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/build/CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_Group.c.o"
+  "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/mpjdev_natmpjdev_Intercomm.c" "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/build/CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_Intercomm.c.o"
+  "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/mpjdev_natmpjdev_Intracomm.c" "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/build/CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_Intracomm.c.o"
+  "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/mpjdev_natmpjdev_NativeRecvRequest.c" "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/build/CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_NativeRecvRequest.c.o"
+  "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/mpjdev_natmpjdev_NativeSendRequest.c" "/home/shravan/mpj-v0_44/src/mpjdev/natmpjdev/lib/build/CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_NativeSendRequest.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/lib/jvm/java-9-openjdk-amd64/include"
+  "/usr/lib/jvm/java-9-openjdk-amd64/include/linux"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lib/mpj-v0_44/src/mpjdev/natmpjdev/lib/build/CMakeFiles/nativempjdev.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_Comm.c.o"
+  "CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_Group.c.o"
+  "CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_NativeRecvRequest.c.o"
+  "CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_NativeSendRequest.c.o"
+  "CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_Intracomm.c.o"
+  "CMakeFiles/nativempjdev.dir/mpjdev_natmpjdev_Intercomm.c.o"
+  "libnativempjdev.pdb"
+  "libnativempjdev.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/nativempjdev.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
